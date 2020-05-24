4a0895a125d43d1899b6562f2b50d265080cd142
long <maulong305@gmail.com>
2020-05-24 12:05:52 +0700

    Modul 1 sắp xếp lại

---
+++ Tuan_1/khaibaobien.js
@@ -0,0 +1,8 @@
+let inputWidth1;
+let inputHeigth1;
+inputWidth1 = prompt("enter the width1")
+inputHeigth1 = prompt('enter the height1')
+let width1 = parseInt(inputWidth1);
+let heigth1 = parseInt(inputHeigth1);
+let area1 = width1 * heigth1;
+document.write('the area1 is:' + area1)
